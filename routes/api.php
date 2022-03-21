a77afd172a0d9c00135691358c7adbe1c7591c9d
Nur Insan Akbar <65167903+akbardevs@users.noreply.github.com>
2022-03-21 16:52:15 +0800

    testing

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,8 @@ Route::get("/connect", [UserController::class, 'testDB']);
 Route::get("list/logs", [UserController::class, 'listStorage']);
 Route::get("/clear/logs", [UserController::class, 'clearLog']);
 
+Route::get("/testing/fun", [UserController::class, 'testingaja']);
+
 Route::post("/testingcallback", [ProductController::class, 'testingcallback']);
 Route::middleware(['basicAuth'])->group(function () {
 	Route::get("/kantin/list", [KasirController::class, 'list']);
